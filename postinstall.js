541be28a12435584f626abcf8693c5beb1d38628
alexbowen <alexbowen123@googlemail.com>
2015-11-25 14:52:13 +0000

    added templating of version

--- postinstall.js
+++ postinstall.js
@@ -3,10 +3,11 @@
 'use strict';
 
 var appRoot = require('app-root-path');
+var template = require('lodash/string/template');
 
 var moduleRoot = process.cwd();
 
-var fs = require('fs-extra');
+var fs = require('fs');
 var chalk = require('chalk');
 
 var fearDependencies = require('../../package.json').fearDependencies;
@@ -15,14 +16,11 @@ var fearCoreTasks = chalk.cyan('Installing FEAR:');
 
 copyDefaultToAppRoot('jspm.conf.js', 'app/scripts/jspm.conf.js');
 
-var template = require('lodash/string/template');
+var execSync = require('child_process').execSync;
 
-
-//var execSync = require('child_process').execSync;
-//
-//execSync('cd ' + appRoot.path + '; npm install digitalinnovation/fear-core-tasks#' + fearDependencies.tasks + ' --save-dev; npm install digitalinnovation/fear-core-serve#' + fearDependencies.serve + ' --save;', {
-//    stdio: 'inherit'
-//});
+execSync('cd ' + appRoot.path + '; npm install digitalinnovation/fear-core-tasks#' + fearDependencies.tasks + ' --save-dev; npm install digitalinnovation/fear-core-serve#' + fearDependencies.serve + ' --save;', {
+    stdio: 'inherit'
+});
 
 function copyDefaultToAppRoot (srcFilename, dstFilename) {
 
@@ -30,10 +28,9 @@ function copyDefaultToAppRoot (srcFilename, dstFilename) {
     var dst = appRoot + '/' + dstFilename;
 
     try {
-
-        // clobber: false means fs-extra will throw an error if the dst exists
-        var ws = fs.createOutputStream(dst);
-        ws.write(template( src, { version: '1.0.0' }));
+        var templateContent = fs.readFileSync(src).toString();
+        var toCompile = template(templateContent);
+        fs.writeFileSync(dst, toCompile({ version: '1.0.0' }));
 
         logCopyOk(dstFilename);
 
