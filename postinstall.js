b77035627df061007a9df7e7032a5eeb3cf29c66
alexbowen <alexbowen123@googlemail.com>
2015-11-26 13:11:13 +0000

    minor refactoring

--- postinstall.js
+++ postinstall.js
@@ -3,34 +3,21 @@
 'use strict';
 
 var appRoot = require('app-root-path');
-var template = require('lodash/string/template');
 
 var moduleRoot = process.cwd();
 
 var fs = require('fs');
 var chalk = require('chalk');
 
-var fearDependencies = require('../../package.json').fearDependencies;
-
 var fearCoreTasks = chalk.cyan('Installing FEAR:');
+
+/**
+ * Copy versioned files to project root
+ */
+var template = require('lodash/string/template');
 
 copyDefaultToAppRoot('jspm.conf.js', 'app/scripts/jspm.conf.js');
 copyDefaultToAppRoot('scripts.html', 'app/views/default/modules/scripts.html');
-
-var execSync = require('child_process').execSync;
-
-execSync('cd ' + appRoot.path);
-
-var installModules = require('npm-install-modules');
-
-var opts = {
-    dependencies: ["digitalinnovation/fear-core-serve#" + fearDependencies.serve],
-    devDependencies: ["digitalinnovation/fear-core-tasks#" + fearDependencies.tasks]
-};
-
-installModules(opts, function () {
-    console.log('DONE');
-});
 
 function copyDefaultToAppRoot (srcFilename, dstFilename) {
 
@@ -67,4 +54,19 @@ function logCopyError (filename, err) {
     console.log(fearCoreTasks+chalk.red(' cannot copy '+filename)+'\nError: '+err.message+'\n');
 }
 
-//npm install digitalinnovation/fear-core-tasks#1.0.0 --save-dev; npm install digitalinnovation/fear-core-serve#1.0.0 --save;
+/**
+ * Install Fear core versioned modules
+ */
+var execSync = require('child_process').execSync;
+
+execSync('cd ' + appRoot.path);
+
+var installModules = require('npm-install-modules');
+var fearDependencies = require('../../package.json').fearDependencies;
+
+var opts = {
+    dependencies: ["digitalinnovation/fear-core-serve#" + fearDependencies.serve],
+    devDependencies: ["digitalinnovation/fear-core-tasks#" + fearDependencies.tasks]
+};
+
+installModules(opts);
