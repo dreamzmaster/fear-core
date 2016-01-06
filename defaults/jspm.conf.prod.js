09e0c6b624aa98ccb6b416f665f08062b79c7e66
alexbowen <alexbowen123@googlemail.com>
2016-01-06 14:30:36 +0000

    jspm fixes

--- defaults/jspm.conf.prod.js
+++ defaults/jspm.conf.prod.js
@@ -1,12 +1,19 @@
+/*eslint-disable */
 System.config({
     baseURL: "/",
     defaultJSExtensions: true,
-    transpiler: "traceur",
+    transpiler: "babel",
+    babelOptions: {
+        "optional": [
+            "runtime",
+            "optimisation.modules.system"
+        ]
+    },
     paths: {
-        "common/*": "common/*.js",
-        "browse/*": "browse/*.js",
-        "bandc/*": "bandc/*.js",
-        "core/*": "jspm_components/github/DigitalInnovation/fear-core-app@1.0.0/*.js",
+        "common/*": mnsScriptLocation + "common/*.js",
+        "browse/*": mnsScriptLocation + "browse/*.js",
+        "bandc/*": mnsScriptLocation + "bandc/*.js",
+        "core/*": mnsScriptLocation + "jspm_components/github/DigitalInnovation/fear-core-app@1.0.0/*.js",
         "github:*": "jspm_components/github/*",
         "npm:*": "jspm_components/npm/*"
     },
@@ -37,12 +44,7 @@ System.config({
         ],
         "angular-ui-router": [
             "angular"
-        ],
-        "bandc/scripts/lib/jquery.signalr": {
-            "deps": [
-                "bandc/scripts/lib/jquery"
-            ]
-        }
+        ]
     },
 
     meta: {
@@ -62,16 +64,13 @@ System.config({
         "angular-route": "github:angular/bower-angular-route@1.2.28",
         "angular-sanitize": "github:angular/bower-angular-sanitize@1.2.28",
         "angular-ui-router": "github:DigitalInnovation/ui-router@0.2.15",
-        "app": "github:DigitalInnovation/fear-core-app@1.0.0/scripts/app",
-        "babel": "npm:babel-core@5.8.33",
-        "babel-runtime": "npm:babel-runtime@5.8.29",
-        "core-js": "npm:core-js@1.2.5",
+        "babel": "npm:babel-core@5.8.25",
+        "babel-runtime": "npm:babel-runtime@5.8.24",
+        "core-js": "npm:core-js@1.1.4",
         "fastclick": "github:ftlabs/fastclick@1.0.3",
-        "fear-core-app": "github:DigitalInnovation/fear-core-app@1.0.0",
-        "moment": "github:moment/moment@2.10.6",
         "packages/angular.package": "github:DigitalInnovation/fear-core-app@1.0.0/scripts/packages/angular.package",
+        "moment": "github:moment/moment@2.10.6",
         "signalr.hubs": "http://localhost:9100/signalr/hubs?",
-        "text": "github:systemjs/plugin-text@0.0.3",
         "traceur": "github:jmcriffey/bower-traceur@0.0.91",
         "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
         "underscore": "github:jashkenas/underscore@1.8.2",
@@ -79,42 +78,17 @@ System.config({
         "github:angular/bower-angular-mocks@1.2.28": {
             "angular": "github:angular/bower-angular@1.2.28"
         },
-        "github:jspm/nodelibs-assert@0.1.0": {
-            "assert": "npm:assert@1.3.0"
+        "github:jspm/nodelibs-process@0.1.1": {
+            "process": "npm:process@0.10.1"
         },
-        "github:jspm/nodelibs-path@0.1.0": {
-            "path-browserify": "npm:path-browserify@0.0.0"
+        "npm:babel-runtime@5.8.24": {
+            "process": "github:jspm/nodelibs-process@0.1.1"
         },
-        "github:jspm/nodelibs-process@0.1.2": {
-            "process": "npm:process@0.11.2"
-        },
-        "github:jspm/nodelibs-util@0.1.0": {
-            "util": "npm:util@0.10.3"
-        },
-        "npm:assert@1.3.0": {
-            "util": "npm:util@0.10.3"
-        },
-        "npm:babel-runtime@5.8.29": {
-            "process": "github:jspm/nodelibs-process@0.1.2"
-        },
-        "npm:core-js@1.2.5": {
+        "npm:core-js@1.1.4": {
             "fs": "github:jspm/nodelibs-fs@0.1.2",
-            "path": "github:jspm/nodelibs-path@0.1.0",
-            "process": "github:jspm/nodelibs-process@0.1.2",
+            "process": "github:jspm/nodelibs-process@0.1.1",
             "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-        },
-        "npm:inherits@2.0.1": {
-            "util": "github:jspm/nodelibs-util@0.1.0"
-        },
-        "npm:path-browserify@0.0.0": {
-            "process": "github:jspm/nodelibs-process@0.1.2"
-        },
-        "npm:process@0.11.2": {
-            "assert": "github:jspm/nodelibs-assert@0.1.0"
-        },
-        "npm:util@0.10.3": {
-            "inherits": "npm:inherits@2.0.1",
-            "process": "github:jspm/nodelibs-process@0.1.2"
         }
     }
 });
+/*eslint-enable */
