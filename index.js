49632955e4b2569a300ca280c03578350a0d8907
alexbowen <alexbowen123@googlemail.com>
2016-01-25 15:47:08 +0000

    changed index.js module names

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ var fearModules = [];
 
 for (d in fearDeps.dependencies) {
     if (fearDeps.dependencies.hasOwnProperty(d)) {
-        fearModules[d] = require('fear-core-' + d + '-tasks');
+        fearModules[d] = require('fear-core-' + d);
     }
 }
 
 for (d in fearDeps.devDependencies) {
     if (fearDeps.devDependencies.hasOwnProperty(d)) {
-        fearModules[d] = require('fear-core-' + d + '-tasks');
+        fearModules[d] = require('fear-core-' + d);
     }
 }
 
