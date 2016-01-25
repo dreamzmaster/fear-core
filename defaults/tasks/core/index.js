43afd93b622d6939c9d88d2b9e635aebec58da18
alexbowen <alexbowen123@googlemail.com>
2016-01-25 16:18:16 +0000

    added check for parent app

--- defaults/tasks/core/index.js
+++ defaults/tasks/core/index.js
@@ -19,16 +19,17 @@ module.exports = function () {
 
     /**
      * create-app-config
+     * create application config for runtime
      */
     gulp.task('create-app-config', function () {
 
         var gulp = require('gulp');
         var gutil = require('gulp-util');
-        var Readable = require('stream').Readable,
-            src = new Readable({
-                objectMode: true
-            }),
-            content = config.getAppConfigTpl().replace('__JSON_CONFIG__', JSON.stringify(config.get('app')));
+        var Readable = require('stream').Readable;
+        var content = config.getAppConfigTpl().replace('__JSON_CONFIG__', JSON.stringify(config.get('app')));
+        var src = new Readable({
+            objectMode: true
+        });
 
         // create source file content
         src._read = function () {
--- postinstall.js
+++ postinstall.js
@@ -1,69 +1,77 @@
 'use strict';
 
-var appRoot = require('app-root-path');
-var moduleRoot = process.cwd();
-var fs = require('fs-extra');
-var path = require('path');
-var utils = require('./utils');
-var fearDeps = require('../../package.json').fear;
+var fs = require('fs');
 
-/**
- * load paths
- */
-var paths = require('./defaults/config/default/paths');
+fs.exists('../../package.json', function (parentAppExists) {
 
-utils.fs.copy('./defaults/config', path.join(appRoot.path, 'config'), false);
-utils.fs.copy('./defaults/tasks', path.join(appRoot.path, 'tasks'), false);
-utils.fs.copy('./defaults/tasks/core', path.join(appRoot.path, 'tasks/core'), true);
-utils.fs.copy('./defaults/mock', path.join(appRoot.path, 'mock'), false);
-utils.fs.copy('./defaults/gulpfile.js', path.join(appRoot.path, 'gulpfile.js'), false);
+    if (parentAppExists) {
 
-/**
- * Write versioned files to project root
- */
-var templateData = { appVersion: fearDeps.jspm.app };
+        var appRoot = require('app-root-path');
+        var moduleRoot = process.cwd();
+        var path = require('path');
+        var utils = require('./utils');
+        var fearDeps = require('../../package.json').fear;
 
-Promise.all([
-    utils.fs.write(
-        utils.fs.template(moduleRoot + '/defaults/jspm.conf.js', templateData),
-        path.join(appRoot.path, paths.app.base + '/common/scripts/jspm.conf.js')
-    ),
-    utils.fs.write(
-        utils.fs.template(moduleRoot + '/defaults/jspm.conf.test.js', templateData),
-        path.join(appRoot.path, 'test/jspm.conf.js')
-    ),
-    utils.fs.write(
-        utils.fs.template(moduleRoot + '/defaults/jspm.conf.prod.js', templateData),
-        path.join(appRoot.path, 'config/integrated/jspm.conf.js')
-    )
-]).then(function () {
+        /**
+         * load paths
+         */
+        var paths = require('./defaults/config/default/paths');
 
-    /**
-     * Install Fear core versioned modules
-     */
-    var installModules = require('npm-install-modules');
+        utils.fs.copy('./defaults/config', path.join(appRoot.path, 'config'), false);
+        utils.fs.copy('./defaults/tasks', path.join(appRoot.path, 'tasks'), false);
+        utils.fs.copy('./defaults/tasks/core', path.join(appRoot.path, 'tasks/core'), true);
+        utils.fs.copy('./defaults/mock', path.join(appRoot.path, 'mock'), false);
+        utils.fs.copy('./defaults/gulpfile.js', path.join(appRoot.path, 'gulpfile.js'), false);
 
-    var opts = {
-        devDependencies: [],
-        dependencies : []
-    };
+        /**
+         * Write versioned files to project root
+         */
+        var templateData = {appVersion: fearDeps.jspm.app};
 
-    var d;
+        Promise.all([
+            utils.fs.write(
+                utils.fs.template(moduleRoot + '/defaults/jspm.conf.js', templateData),
+                path.join(appRoot.path, paths.app.base + '/common/scripts/jspm.conf.js')
+            ),
+            utils.fs.write(
+                utils.fs.template(moduleRoot + '/defaults/jspm.conf.test.js', templateData),
+                path.join(appRoot.path, 'test/jspm.conf.js')
+            ),
+            utils.fs.write(
+                utils.fs.template(moduleRoot + '/defaults/jspm.conf.prod.js', templateData),
+                path.join(appRoot.path, 'config/integrated/jspm.conf.js')
+            )
+        ]).then(function () {
 
-    for (d in fearDeps.dependencies) {
-        if (fearDeps.dependencies.hasOwnProperty(d)) {
-            opts.dependencies.push('fear-core-' + d + (fearDeps.dependencies[d] !== 'latest' ? '@' + fearDeps.dependencies[d] : ''));
-        }
-    }
+            /**
+             * Install Fear core versioned modules
+             */
+            var installModules = require('npm-install-modules');
 
-    if (!process.env.NODE_ENV) {
-        for (d in fearDeps.devDependencies) {
-            if (fearDeps.devDependencies.hasOwnProperty(d)) {
-                opts.devDependencies.push('fear-core-' + d + (fearDeps.devDependencies[d] !== 'latest' ? '@' + fearDeps.devDependencies[d] : ''));
+            var opts = {
+                devDependencies: [],
+                dependencies: []
+            };
+
+            var d;
+
+            for (d in fearDeps.dependencies) {
+                if (fearDeps.dependencies.hasOwnProperty(d)) {
+                    opts.dependencies.push('fear-core-' + d + (fearDeps.dependencies[d] !== 'latest' ? '@' + fearDeps.dependencies[d] : ''));
+                }
             }
-        }
-    }
 
-    installModules(opts, function () {});
+            if (!process.env.NODE_ENV) {
+                for (d in fearDeps.devDependencies) {
+                    if (fearDeps.devDependencies.hasOwnProperty(d)) {
+                        opts.devDependencies.push('fear-core-' + d + (fearDeps.devDependencies[d] !== 'latest' ? '@' + fearDeps.devDependencies[d] : ''));
+                    }
+                }
+            }
+
+            installModules(opts, function () {
+            });
+        });
+    }
 });
 
